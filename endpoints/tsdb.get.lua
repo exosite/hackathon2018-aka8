443458ebf6d140b2614aff09c7f8a7fdbd982553
Qimat Luo <qimatluo@exosite.com>
2018-08-13 14:59:03 +0800

    Update tsdb

--- endpoints/tsdb.get.lua
+++ endpoints/tsdb.get.lua
@@ -1,8 +1,6 @@
 --#ENDPOINT GET /tsdb
 
-local p = request.parameters
-local R = require('moses')
-if R.isString(p.metrics) then
-	p.metrics = {p.metrics}
-end
-response.message = Tsdb.query(p)
+response.message = {
+	Tsdb.query(from_json(request.parameters.query)),
+	from_json(request.parameters.query),
+}
