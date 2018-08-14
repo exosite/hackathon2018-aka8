1fca62d837a75b9c28c0c360e581a417f1df621e
exosite-KittyHo <kittyho@exosite.com>
2018-08-14 12:22:58 +0800

    Testing

--- test/e2e/AKA8Test.py
+++ test/e2e/AKA8Test.py
@@ -13,7 +13,7 @@ class AKA8Test(unittest.TestCase):
         self.CacheIdKeys = 'https://aka8.apps.exosite.io/cache/{id}/{keys}'
         self.CacheIdKey = 'https://aka8.apps.exosite.io/cache/{id}/{key}'
         self.CacheId = 'https://aka8.apps.exosite.io/cache/{id}'
-        self.Tsdb = 'https://aka8.apps.exosite.io/tsdb'
+        self.Tsdb = 'https://aka8.apps.exosite.io/tsdb/{query}'
         self.headers = {
             'content-type': 'application/json',
         }
@@ -36,11 +36,8 @@ class AKA8Test(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_GetTsdb(self):
-        params = (
-            ('query', '{"metrics":["H01"], "limit":10}'),
-        )
         response = requests.get(
-            self.Tsdb, params=params, headers=self.headers)
+            self.Tsdb.format(query='{"metrics":["H01"], "limit":10}'), headers=self.headers)
         self.assertEqual(response.status_code, 200)
 
     def test_PostCacheIdKey_humidity(self):
@@ -61,12 +58,8 @@ class AKA8Test(unittest.TestCase):
         self.assertEqual(old_count+1, check_count)
         self.assertEqual(check_sum, new_sum)
         self.assertEqual(response.status_code, 200)
-<<<<<<< HEAD
 
     def test_PostCacheIdKey_temp(self):
-=======
-     def test_PostCacheIdKey_temp(self):
->>>>>>> Test
         resp = requests.get(self.CacheIdKeys.format(
             id='qimat', keys='temp'), headers=self.headers)
         dict = ast.literal_eval(resp.content).get('temp')
